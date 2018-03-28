fd737f157b8bfdb339fa3ef20f500bedbac5ced1
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-28 21:01:48 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ class Person
   :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attr)
-    attr.each {|key,value| self.send(("#{key}"),value)}
+    attr.each {|key,value| self.send(("#{key}="),value)}
   end
 
 end
